9e18be5775e1f63f981835e8677fbcc913900236
min hyeok Jeong <simneol09784@gmail.com>
2016-11-06 23:05:27 +0900

    change source code into general

--- check_eclass.py
+++ check_eclass.py
@@ -1,195 +1,65 @@
-import urllib, http.cookiejar, ssl, codecs, subprocess, time
+#!/usr/bin/python3
+import codecs
+import http.cookiejar
+import os
+import ssl
+import time
+import re
+import urllib
 from bs4 import BeautifulSoup
 
-#### LOGIN INFO ####
-login_info={
-        'userDTO.userId':'YourID',
-        'userDTO.password':'YourPW'
+MAIN_URL = 'http://eclass.dongguk.edu/'
+PARSER = 'lxml'
+
+if os.name == 'nt':
+    PARSER = 'html.parser'
+
+# LOGIN INFO
+LOGIN_URL = 'https://eclass.dongguk.edu/User.do?cmd=loginUser'
+LOGIN_INFO = {
+        'userDTO.userId': 'YourID',
+        'userDTO.password': 'YourPW'
         }
 
-#### LOGIN OPERATION ####
-cj=http.cookiejar.LWPCookieJar()
-https_sslv23_handler=urllib.request.HTTPSHandler(context=ssl.SSLContext(ssl.PROTOCOL_SSLv23))
-opener=urllib.request.build_opener(https_sslv23_handler,urllib.request.HTTPCookieProcessor(cj))
-urllib.request.install_opener(opener)
-login_url='https://eclass.dongguk.edu/User.do?cmd=loginUser'
+# LOGIN OPERATION
+cookie_jar = http.cookiejar.LWPCookieJar()
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+ssl_handler = urllib.request.HTTPSHandler(context=ssl_context)
 
-login_request=urllib.parse.urlencode(login_info)
-req=urllib.request.Request(login_url,login_request.encode('UTF-8'))
+cj_processor = urllib.request.HTTPCookieProcessor(cookie_jar)
+opener = urllib.request.build_opener(ssl_handler, cj_processor)
+urllib.request.install_opener(opener)
+
+login_request = urllib.parse.urlencode(LOGIN_INFO)
+req = urllib.request.Request(LOGIN_URL, login_request.encode('UTF-8'))
 res = urllib.request.urlopen(req)
 
-#### UPDATE HISTORY FILE (IF NOT EXIST, CREATE) ####
-subprocess.call(["touch","prev_assign1.txt"])
-subprocess.call(["touch","prev_assign2.txt"])
-subprocess.call(["touch","prev_assign3.txt"])
-subprocess.call(["touch","prev_assign4.txt"])
-subprocess.call(["touch","prev_assign5.txt"])
-subprocess.call(["touch","prev_assign6.txt"])
-subprocess.call(["touch","prev_assign7.txt"])
+# GET STUDY MAIN PAGE
+url1 = 'http://eclass.dongguk.edu/Study.do?cmd=viewMyCourse&curriculumType=degree&studyAuthor=class_learner'
+res = opener.open(url1)
+data = res.read().decode('utf-8')
+soup = BeautifulSoup(data, PARSER)
 
+html_semesterform = soup.find('span', {'class': 'selectForm'})
+html_semesterlist = html_semesterform.find('span', {'class': 'list'})
+html_semesteritems = html_semesterlist.find_all('a')
 
-#### LOOP INFINITELY ####
-while 1:
-    #### No.1 ####
-    url1 = 'http://eclass.dongguk.edu/Board.do?cmd=viewBoardContents&boardInfoDTO.boardInfoId=S2016U0002001UCSE404401Q&boardContentsDTO.boardContentsId=BOAD_160312A143056I5e4fdc&boardContentsDTO.contentsNo=7&boardContentsDTO.gongjiYn=N&gubun=V&curPage=1&typeYn=Y'
+semester_list = []
+semester_href = []
+semester_name_regex = re.compile(r'[\s\S]*([0-9]{4}년도 .*학기)[\s\S]*')
+for item in html_semesteritems:
+    semester_list.append(semester_name_regex.sub(r'\1', item.text))
+    semester_href.append(item['href'])
 
-    res = opener.open(url1)
-    data=res.read().decode('utf-8')
-    soup=BeautifulSoup(data,"lxml")
-    comlists=soup.find_all('div',{'class':'section_gudieline_frame'})
-
-    content=''
-    for comment in comlists:
-        content+=comment.text
-
-    fr=codecs.open('prev_assign1.txt','r+','utf-8')
-    if content!=fr.read():
-        as1=1
-    else:
-        as1=0
-    fr.close()
-    fw=codecs.open('prev_assign1.txt','w','utf-8')
-    fw.write(content)
-    fw.close()
-
-
-    #### No.2 ####
-    url2 = 'http://eclass.dongguk.edu/Board.do?cmd=viewBoardContents&boardInfoDTO.boardInfoId=S2016U0002001UCSE404401Q&boardContentsDTO.boardContentsId=BOAD_160312A143038I5e4fdb&boardContentsDTO.contentsNo=6&boardContentsDTO.gongjiYn=N&gubun=V&curPage=1&typeYn=Y'
-
-    res = opener.open(url2)
-    data=res.read().decode('utf-8')
-    soup=BeautifulSoup(data,"lxml")
-    comlists=soup.find_all('div',{'class':'section_gudieline_frame'})
-
-    content=''
-    for comment in comlists:
-        content+=comment.text
-
-    fr=codecs.open('prev_assign2.txt','r+','utf-8')
-    if content!=fr.read():
-        as2=1
-    else:
-        as2=0
-    fr.close()
-    fw=codecs.open('prev_assign2.txt','w','utf-8')
-    fw.write(content)
-    fw.close()
-
-    #### No.3 ####
-    url3 = 'http://eclass.dongguk.edu/Board.do?cmd=viewBoardContents&boardInfoDTO.boardInfoId=S2016U0002001UCSE404401Q&boardContentsDTO.boardContentsId=BOAD_160312A143021I5e4fda&boardContentsDTO.contentsNo=5&boardContentsDTO.gongjiYn=N&gubun=V&curPage=1&typeYn=Y'
-
-    res = opener.open(url3)
-    data=res.read().decode('utf-8')
-    soup=BeautifulSoup(data,"lxml")
-    comlists=soup.find_all('div',{'class':'section_gudieline_frame'})
-
-    content=''
-    for comment in comlists:
-        content+=comment.text
-
-    fr=codecs.open('prev_assign3.txt','r+','utf-8')
-    if content!=fr.read():
-        as3=1
-    else:
-        as3=0
-    fr.close()
-    fw=codecs.open('prev_assign3.txt','w','utf-8')
-    fw.write(content)
-    fw.close()
-
-    #### No.4 ####
-    url4 = 'http://eclass.dongguk.edu/Board.do?cmd=viewBoardContents&boardInfoDTO.boardInfoId=S2016U0002001UCSE404401Q&boardContentsDTO.boardContentsId=BOAD_160312A143001I5e4fd9&boardContentsDTO.contentsNo=4&boardContentsDTO.gongjiYn=N&gubun=V&curPage=1&typeYn=Y'
-
-    res = opener.open(url4)
-    data=res.read().decode('utf-8')
-    soup=BeautifulSoup(data,"lxml")
-    comlists=soup.find_all('div',{'class':'section_gudieline_frame'})
-
-    content=''
-    for comment in comlists:
-        content+=comment.text
-
-    fr=codecs.open('prev_assign4.txt','r+','utf-8')
-    if content!=fr.read():
-        as4=1
-    else:
-        as4=0
-    fr.close()
-    fw=codecs.open('prev_assign4.txt','w','utf-8')
-    fw.write(content)
-    fw.close()
-
-    #### No.5 ####
-
-    url5 = 'http://eclass.dongguk.edu/Board.do?cmd=viewBoardContents&boardInfoDTO.boardInfoId=S2016U0002001UCSE404401Q&boardContentsDTO.boardContentsId=BOAD_160312A142935I5e4fd3&boardContentsDTO.contentsNo=3&boardContentsDTO.gongjiYn=N&gubun=V&curPage=1&typeYn=Y'
-
-    res = opener.open(url5)
-    data=res.read().decode('utf-8')
-    soup=BeautifulSoup(data,"lxml")
-    comlists=soup.find_all('div',{'class':'section_gudieline_frame'})
-
-    content=''
-    for comment in comlists:
-        content+=comment.text
-
-    fr=codecs.open('prev_assign5.txt','r+','utf-8')
-    if content!=fr.read():
-        as5=1
-    else:
-        as5=0
-    fr.close()
-    fw=codecs.open('prev_assign5.txt','w','utf-8')
-    fw.write(content)
-    fw.close()
-
-    #### No.6 ####
-    url6 = 'http://eclass.dongguk.edu/Board.do?cmd=viewBoardContents&boardInfoDTO.boardInfoId=S2016U0002001UCSE404401Q&boardContentsDTO.boardContentsId=BOAD_160312A142917I5e4fd2&boardContentsDTO.contentsNo=2&boardContentsDTO.gongjiYn=N&gubun=V&curPage=1&typeYn=Y'
-
-    res = opener.open(url6)
-    data=res.read().decode('utf-8')
-    soup=BeautifulSoup(data,"lxml")
-    comlists=soup.find_all('div',{'class':'section_gudieline_frame'})
-
-    content=''
-    for comment in comlists:
-        content+=comment.text
-
-    fr=codecs.open('prev_assign6.txt','r+','utf-8')
-    if content!=fr.read():
-        as6=1
-    else:
-        as6=0
-    fr.close()
-    fw=codecs.open('prev_assign6.txt','w','utf-8')
-    fw.write(content)
-    fw.close()
-
-    #### No.7 ####
-    url7 = 'http://eclass.dongguk.edu/Board.do?cmd=viewBoardContents&boardInfoDTO.boardInfoId=S2016U0002001UCSE404401Q&boardContentsDTO.boardContentsId=BOAD_160312A142851I5e4fd1&boardContentsDTO.contentsNo=1&boardContentsDTO.gongjiYn=N&gubun=V&curPage=1&typeYn=Y'
-
-    res = opener.open(url7)
-    data=res.read().decode('utf-8')
-    soup=BeautifulSoup(data,"lxml")
-    comlists=soup.find_all('div',{'class':'section_gudieline_frame'})
-
-    content=''
-    for comment in comlists:
-        content+=comment.text
-
-    fr=codecs.open('prev_assign7.txt','r+','utf-8')
-    if content!=fr.read():
-        as7=1
-    else:
-        as7=0
-    fr.close()
-    fw=codecs.open('prev_assign7.txt','w','utf-8')
-    fw.write(content)
-    fw.close()
-
-    ## Debug Message ##
-    #print('check OK / code :'+str(as1)+str(as2)+str(as3)+str(as4)+str(as5)+str(as6)+str(as7))
-
-    ## Send Message ##
-    if as1 or as2 or as3 or as4 or as5 or as6 or as7:
-        subprocess.call(["../send_telegram.sh","민혁_정","Some Assignment was changed!"+str(as1)+str(as2)+str(as3)+str(as4)+str(as5)+str(as6)+str(as7), ""])
-    time.sleep(10)
+for name, href in zip(semester_list, semester_href):
+    print('\n' + name + '에 수강했던 강의 목록\n')
+    res = opener.open(MAIN_URL + href)
+    data = res.read().decode('utf-8')
+    soup = BeautifulSoup(data, PARSER)
+    course_list = soup.find_all('div', {'class', 'mystudy_directory first'})
+    for course in course_list:
+        t = course.find('h4').find('span').text
+        b = re.sub(r'\n', r'', t)
+        c = re.sub(r'\s*(\S.*)', r'\1', b)
+        print(c)
+    print('\n' + '-' * 20)
